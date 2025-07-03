db451e08be6bd07e0d8daf8f98fc5f4061a887a2
MuathMunif <moath.almutairi96@gmail.com>
2025-07-03 19:01:21 +0300

    complate the game with rounds still the error handeler

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,81 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int playerWinns = 0;
-        int computerWinns = 0;
+        int copmuterWinss = 0;
         System.out.println("tic tac toe game  ");
-
-
-        char [][] board = {
-                {' ', ' ', ' '},
-                {' ', ' ', ' '},
-                {' ', ' ', ' '}
-        };
-        while (true){
-            printBoard(board);
-            playerMove(board, 'x');
-            if(checkWinner(board, 'x')){
+        System.out.println("Enter 1 if you want one round :");
+        System.out.println("Enter 2 if you want three rounds :");
+        int chose = scanner.nextInt();
+        if (chose == 1) {
+            while (true) {
+                char[][] board = {
+                        {' ', ' ', ' '},
+                        {' ', ' ', ' '},
+                        {' ', ' ', ' '}
+                };
                 printBoard(board);
-                System.out.println("The player X wins !");
-                break;
+                playerMove(board, 'x');
+                if (checkWinner(board, 'x')) {
+                    printBoard(board);
+                    System.out.println("The player X wins !");
+                    break;
+                }
+                computerMove(board, 'O');
+                if (checkWinner(board, 'o')) {
+                    printBoard(board);
+                    System.out.println("The player 0 wins !");
+                    break;
+                }
+
             }
-            computerMove(board,'O');
-             if(checkWinner(board, 'o')){
-                printBoard(board);
-                System.out.println("The player 0 wins !");
-                break;
+        } else if (chose == 2) {
+            int rounds = 3;
+            while(rounds>0) {
+                char[][] board = {
+                        {' ', ' ', ' '},
+                        {' ', ' ', ' '},
+                        {' ', ' ', ' '}
+                };
+                while (true) {
+                    printBoard(board);
+                    playerMove(board, 'x');
+                    if (checkWinner(board, 'x')) {
+                        printBoard(board);
+                        System.out.println("The player wins The round !");
+                        playerWinns++;
+                        break;
+                    }
+                    computerMove(board, 'O');
+                    if (checkWinner(board, 'o')) {
+                        printBoard(board);
+                        System.out.println("The Computer wins The round!");
+                        copmuterWinss++;
+                        break;
+                    }
+
+                }
+                System.out.println("players winns "+ playerWinns);
+                System.out.println("computer winns "+copmuterWinss);
+                rounds--;
+                if (playerWinns == 2){
+                    System.out.println("The player winns The game ");
+                    System.out.println("The End");
+                    break;
+                }
+                else if (copmuterWinss == 2){
+                        System.out.println("The player winns The game ");
+                        System.out.println("The End");
+                        break;
+                }
+
             }
 
         }
     } // out of the main
-    public static void printBoard(char [][] arr){
+
+    public static void printBoard(char[][] arr) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(arr[i][j]);
@@ -44,48 +91,47 @@ public class Main {
             }
         }
     }
-    public static void playerMove(char[][] board , char player){
+
+    public static void playerMove(char[][] board, char player) {
         Scanner scanner = new Scanner(System.in);
-        int row ;
+        int row;
         int col;
-        while (true){
+        while (true) {
             System.out.println("Enter row (0, 1, 2): ");
             row = scanner.nextInt();
             System.out.println("Enter column (0, 1, 2): ");
             col = scanner.nextInt();
-            if(row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ' ){
-                board [row][col] = player;
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+                board[row][col] = player;
                 break;
-            }
-            else {
+            } else {
                 System.out.println("the place was chosen !");
             }
 
         }
     }
 
-    public static void computerMove(char[][] board , char computer){
+    public static void computerMove(char[][] board, char computer) {
         Random random = new Random();
-        int row ;
+        int row;
         int col;
-        while (true){
+        while (true) {
             row = random.nextInt(3);
             col = random.nextInt(3);
 
-            if(board[row][col] == ' ' ){
-                board [row][col] = computer;
+            if (board[row][col] == ' ') {
+                board[row][col] = computer;
                 break;
-            }
-            else {
+            } else {
                 System.out.println("the place was chosen !");
             }
 
         }
     }
 
-    public static boolean checkWinner(char[][] board , char check){
-        for (int i = 0; i < 3; i++){
-            if (board[i][0] == check && board[i][1] == check && board[i][2]== check){
+    public static boolean checkWinner(char[][] board, char check) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == check && board[i][1] == check && board[i][2] == check) {
                 return true;
             }
         }
